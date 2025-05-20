0495c4e5becf474ec2a235a9e258e6e88f84544b
ezyybin604 <ezramorales@Victors-MacBook-Air.local>
2025-05-19 17:51:24 -0700

    New update thing?

--- kubejs/server_scripts/example.js
+++ kubejs/server_scripts/example.js
@@ -1,5 +1,20 @@
 // priority: 0
 
+if (!String.prototype.format) {
+String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+    return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+    ;
+    });
+};
+}
+
+// Base off of startup_scripts
+let egg_colors = ["red", "green", "bronze", "gray", "blue", "white", "sapphire", "silver", "electric", "amythest", "copper", "black"]
+
 // Visit the wiki for more info - https://kubejs.com/
 
 console.info('Server Scripts: The Logging:') // tried using mcreator to overwrite the diamond recipes but mcreator was being fussy
@@ -23,9 +38,26 @@ ServerEvents.recipes(event => {
             "results": [{"item": "botania:" + mystical_colors[i] + "_mystical_flower"}]
         })
     }
+    for (let i=0;i<egg_colors.length;i++) {
+        //event.create('broken_{0}_dragon_egg'.format(egg_colors[i])).texture('iceandfire:item/dragonegg_{0}'.format(egg_colors[i]))
+        console.log('iceandfire:dragonegg_{0}'.format(egg_colors[i]))
+        event.shapeless(
+            Item.of('iceandfire:dragonegg_{0}'.format(egg_colors[i]), 1), // arg 1: output
+            [
+                'minecraft:dragon_egg',
+                'iceandfire:dragon_meal',
+                'kubejs:broken_{0}_dragon_egg'.format(egg_colors[i])
+            ]
+        );
+    }
 
     event.remove({ id: 'create:crafting/materials/andesite_alloy' })
     event.remove({ id: 'create:crafting/materials/andesite_alloy_from_zinc' })
     event.remove({ id: 'create:mixing/andesite_alloy' })
     event.remove({ id: 'create:mixing/andesite_alloy_from_zinc' })
+    event.remove({ id: 'tconstruct:smeltery/casting/ender/eye'})
+
+    //event.remove({ id: 'tconstruct:smeltery/casting/diamond/gem_sand_cast'})
+    //event.remove({ id: 'tconstruct:smeltery/casting/diamond/gem_gold_cast'})
+    event.remove({ id: 'tconstruct:smeltery/casting/diamond/block'})
 })
--- kubejs/startup_scripts/example.js
+++ kubejs/startup_scripts/example.js
@@ -1,5 +1,25 @@
+
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+      });
+    };
+  }
+
+let egg_colors = ["red", "green", "bronze", "gray", "blue", "white", "sapphire", "silver", "electric", "amythest", "copper", "black"]
+
 console.log("This")
 WorldgenEvents.remove(event => {
     event.removeFeatureById('vegetal_decoration', ['botania:mystical_flowers'])
     event.removeFeatureById('vegetal_decoration', ['botania:mystical_mushrooms'])
+})
+StartupEvents.registry('item', event => {
+    for (let i=0;i<egg_colors.length;i++) {
+        event.create('broken_{0}_dragon_egg'.format(egg_colors[i])).texture('iceandfire:item/dragonegg_{0}'.format(egg_colors[i]))
+    }
 })
